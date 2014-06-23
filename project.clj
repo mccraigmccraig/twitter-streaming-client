28031e5b9cbcb097d8e0f2b935c5d828dd5f1fff
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2014-06-23 21:07:28 +0100

    update twitter-api and midje deps

--- project.clj
+++ project.clj
@@ -11,9 +11,9 @@
     [org.clojure/tools.macro "0.1.2"]
     [org.clojure/data.json "0.2.2"]
 
-    [twitter-api "0.7.4"]])
+    [twitter-api "0.7.6"]])
 
-(defproject twitter-streaming-client/twitter-streaming-client "0.3.1"
+(defproject twitter-streaming-client/twitter-streaming-client "0.3.2"
   :description "a clojure based client for Twitter's streaming API"
   :url "https://github.com/mccraigmccraig/twitter-streaming-client"
 
@@ -21,17 +21,18 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.0.0"
-  :plugins [[lein-midje "3.0.1"]]
+  :plugins [[lein-midje "3.1.3"]]
 
   :dependencies ~(conj shared '[org.clojure/clojure "1.5.1"])
   :dev-dependencies []
 
-  :aliases {"all" ["with-profile" "dev:1.4,dev:1.5"]}
+  :aliases {"all" ["with-profile" "dev:1.4,dev:1.5,dev:1.6"]}
 
   :profiles {:all {:dependencies ~shared}
 
-             :dev {:dependencies [[midje "1.5.1"]]}
+             :dev {:dependencies [[midje "1.6.3"]]}
              :production {}
 
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
